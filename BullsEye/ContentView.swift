198ad2c747094978848615b40bddfca952f0cab3
Oğuz Karatoruk <oguz.karatoruk@flo.com.tr>
2022-03-15 22:57:30 +0300

    ui improvements

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -10,32 +10,44 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("🎯 PUT THE BULLSEYE AS CLOSE AS YOU CAN 🎯")
-                .padding()
-                .font(.callout)
-            Text("50")
-            HStack {
-                Text("1")
-                    .padding(.leading, 20)
-                    .frame(width: 50)
+            VStack {
+                Text("🎯 PUT THE BULLSEYE AS CLOSE AS YOU CAN 🎯")
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .font(.footnote)
+                Text("50")
+                HStack {
+                    Text("1")
+                        .padding(.leading, 20)
+                        .frame(width: 50)
+                    
                     Slider(value: .constant(50), in: 1.0...100.0)
                         .padding([.leading, .trailing], 20)
-                Text("100")
-                    .padding(.trailing, 20)
-                    .frame(width: 50)
+                    
+                    Text("100")
+                        .padding(.trailing, 20)
+                        .frame(width: 50)
+                }
+                Button(action: {
+                    //action
+                }) {
+                    Text("HIT ME").bold()
+                }.padding()
             }
-            Button("HIT ME!") {
-                //action
-            }.padding()
+            
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .previewLayout(.fixed(width: 812, height: 375))
-            .environment(\.horizontalSizeClass, .compact)
+        Group {
+            ContentView()
+                .previewLayout(.fixed(width: 812, height: 375))
+                .environment(\.horizontalSizeClass, .compact)
             .environment(\.verticalSizeClass, .compact)
+        }
     }
 }
