4a95ed828dae6f8b00d70e070e94fdb6f2d734bd
Oğuz Karatoruk <oguz.karatoruk@flo.com.tr>
2022-03-15 22:27:47 +0300

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Oğuz Karatoruk on 15.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
