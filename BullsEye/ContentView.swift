c189da8c04a0041197c192abbecd1b8ffe78a5cc
Oğuz Karatoruk <oguz.karatoruk@flo.com.tr>
2022-03-15 22:38:52 +0300

    basic of ui

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯 PUT THE BULLSEYE AS CLOSE AS YOU CAN 🎯")
+                .padding()
+                .font(.callout)
+            Text("50")
+            HStack {
+                Text("1")
+                    .padding(.leading, 20)
+                    .frame(width: 50)
+                    Slider(value: .constant(50), in: 1.0...100.0)
+                        .padding([.leading, .trailing], 20)
+                Text("100")
+                    .padding(.trailing, 20)
+                    .frame(width: 50)
+            }
+            Button("HIT ME!") {
+                //action
+            }.padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.fixed(width: 812, height: 375))
+            .environment(\.horizontalSizeClass, .compact)
+            .environment(\.verticalSizeClass, .compact)
     }
 }
